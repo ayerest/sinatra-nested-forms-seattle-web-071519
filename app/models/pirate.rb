cfb0589243114dd1d97b6914a7a2d53580399a11
ayerest <irisiseve@gmail.com>
2019-08-05 16:46:54 -0700

    finished sinatra nested forms lab

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
+require 'pry'
+
 class Pirate
+    attr_accessor :name, :weight, :height
+    @@all = []
+
+    def initialize(args)
+        # binding.pry
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+        
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
